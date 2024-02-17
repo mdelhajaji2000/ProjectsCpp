4ce34f127194ddbb08d815a6012e2cdd52683f1b
Hp Victus de mdelhajaji <mdelhajaijpro2007@gmail.com>
2024-02-17 22:52:37 +0100

    Update

---
+++ git/gitSimplifier.cpp
@@ -0,0 +1,14 @@
+/* Git Commands using C++ Simplifier*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("git status");
+    system("git add *");
+    system("git commit -m \"Update\"");
+    system("git pull");
+    system("git push origin main");
+
+    return 0;
+}
